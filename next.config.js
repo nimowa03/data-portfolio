f51396c4865e60444f87b8544e2fe46024b0fcc5
Charlee <nimowa03@gmail.com>
2025-09-05 14:07:40 +0900

    basePath 설정 제거로 경로 문제 해결

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/data-portfolio',
-  assetPrefix: '/data-portfolio/',
   images: {
     unoptimized: true,
   },
